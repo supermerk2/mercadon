05e4be665d088ec6ced7a10d767c78e3673cfd84
German <72597058+GM-GitH@users.noreply.github.com>
2024-04-07 09:11:13 +0200

    Add schema validation middleware

---
+++ src/middlewares/schemaValidation.middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express"
+import { AnyZodObject, ZodError } from "zod"
+
+export const schemaValidationMiddleware = (schema: AnyZodObject) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({ body: req.body, query: req.query, params: req.params })
+      next()
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return res
+          .status(400)
+          .json(error.issues.map((issue) => ({ message: issue.message })))
+      }
+      return res.status(500).json(error)
+    }
+    return
+  }
+}
